a50c4d5d8621fd6e2acfdc2676acf976639eb2c6
Daniel Różycki <daniel.rozycki@stxnext.pl>
2024-04-05 13:39:53 +0200

    :white_check_mark: Add tests for thread safety

--- tests/test_shared_state.py
+++ tests/test_shared_state.py
@@ -1,6 +1,8 @@
+import threading
+
 from lidipy import Lidi
 
-from tests.shared import SimpleClassA, SimpleClassB, SimpleClassC
+from tests.shared import SimpleClassA, SimpleClassB, SimpleClassC, ThreadService
 
 
 def test_should_not_have_shared_state() -> None:
@@ -19,3 +21,53 @@ def test_should_not_have_shared_state() -> None:
     assert isinstance(resolved_first, SimpleClassB)
     assert isinstance(resolved_second, SimpleClassC)
     assert type(resolved_first) != type(resolved_second)
+
+
+def test_should_be_threadsafe() -> None:
+    # Given
+    lidi = Lidi()
+    lidi.bind(ThreadService, ThreadService)
+
+    def resolve_and_increment() -> None:
+        thread_service = lidi.resolve(ThreadService)
+        thread_service.increment()
+
+    # When
+    num_threads = 10
+    threads = []
+    for _ in range(num_threads):
+        thread = threading.Thread(target=resolve_and_increment)
+        thread.start()
+        threads.append(thread)
+    for thread in threads:
+        thread.join()
+
+    # Then
+    service = lidi.resolve(ThreadService)
+    # Value should be 0, that means no state was shared between threads
+    assert not service.value
+
+
+def test_should_be_threadsafe_for_singletons() -> None:
+    # Given
+    lidi = Lidi()
+    lidi.bind(ThreadService, ThreadService, singleton=True)
+
+    def resolve_and_increment() -> None:
+        thread_service = lidi.resolve(ThreadService)
+        thread_service.increment()
+
+    # When
+    num_threads = 10
+    threads = []
+    for _ in range(num_threads):
+        thread = threading.Thread(target=resolve_and_increment)
+        thread.start()
+        threads.append(thread)
+    for thread in threads:
+        thread.join()
+
+    # Then
+    # Check if the final value of TestService's attribute is equal to the number of threads
+    service = lidi.resolve(ThreadService)
+    assert service.value == num_threads
