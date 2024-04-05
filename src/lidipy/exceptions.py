a395e2a91d9d37eccf8921e486e4953ca2658608
Daniel Różycki <daniel.rozycki@stxnext.pl>
2024-04-05 13:00:23 +0200

    :recycle: Small refactor of existing code

--- src/lidipy/exceptions.py
+++ src/lidipy/exceptions.py
@@ -1,6 +1,16 @@
-class BaseLidiException(Exception):
+"""Exceptions module for the Lidi framework.
+
+Defines custom exceptions used within the Lidi framework.
+"""
+
+
+class BaseLidiException(Exception):  # noqa: N818
+    """Base class for all Lidi exceptions."""
+
     ...
 
 
 class BindingMissing(BaseLidiException):
+    """Raise when binding is not found within Lidi container."""
+
     pass
